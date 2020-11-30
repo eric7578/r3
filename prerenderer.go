e9ba7b6af805c8de3d21afd321ea3f0b2a8b0fc4
eric.tsai <eric.tsai@nexiosoft.com>
2020-11-30 16:44:21 +0800

    Remove redudant code

--- prerenderer.go
+++ prerenderer.go
@@ -12,25 +12,21 @@ import (
 type PrerendererOption struct {
 	Source  string `form:"source" binding:"required"`
 	Timeout int    `form:"timeout,default=30"`
-	Ignores string `form:"ignores"`
 	Repeat  int    `form:"repeat,default=1"`
 	Cache   int    `form:"cache,default=28800"`
 }
 
 type prerenderer struct {
-	PrerendererOption
 }
 
 func (r *prerenderer) render(ctx context.Context, opt PrerendererOption) (html string, err error) {
-	// ignoreElements := strings.Split(opt.Ignores, ",")
-
 	ctx, cancel := context.WithTimeout(ctx, time.Duration(opt.Timeout)*time.Second)
 	defer cancel()
 
 	for opt.Repeat > 0 {
 		html, err = r.fetchPage(ctx, opt.Source)
 		if err != nil {
-			opt.Repeat -= 1
+			opt.Repeat--
 		} else {
 			break
 		}
